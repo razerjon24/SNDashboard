ad5d86ad0dd1821e2d685d253f30f58663cf2271
razerjon24 <razerjon24@gmail.com>
2016-06-01 16:39:39 -0500

    Ejemplo Tutorial Django

--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.views import generic
+from .models import Choice, Question
+class IndexView(generic.ListView):
+   template_name = "DashboardApp/index.html"
+   context_object_name = "latest_question_list"
 
-# Create your views here.
+   def get_queryset(self):
+       return Question.objects.filter(
+           pub_date__lte=timezone.now()
+           ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+   model = Question
+   template_name = "DashboardApp/detail.html"
+
+   def get_queryset(self):
+       """
+       Excludes any questions that aren't published yet.
+       """
+       return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+   model = Question
+   template_name = "DashboardApp/results.html"
+
+   def vote(request, question_id):
+       p = get_object_or_404(Question, pk=question_id)
+       try:
+           selected_choice = p.choice_set.get(pk=request.POST['choice'])
+       except (KeyError, Choice.DoesNotExist):
+           return render(request, 'DashboardApp/detail.html', {
+               'question': p,
+               'error_message': "You didn't select a choice",
+           })
+       else:
+           selected_choice.votes += 1
+           selected_choice.save()
+           return HttpResponseRedirect(reverse('DashboardApp:results', args=(p.id,)))
--- SNDashboard/settings.py
+++ SNDashboard/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DashboardApp',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'SNDashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sndashboard',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
