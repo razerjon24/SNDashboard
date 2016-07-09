a557e264774ae872fcf685a12296eca515ffd3c4
razerjon24 <razerjon24@gmail.com>
2016-07-08 23:29:51 -0500

    Wordcloud in progress

--- DashboardApp/urls.py
+++ DashboardApp/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^heatmap/$', views.HeatMapView.as_view(), name='heatmap'),
     url(r'^heatmap/test/$', views.test, name='test'),
+    url(r'^topic/$', views.TopicView.as_view(), name='topic'),
     # ex: /polls/5/vote/
 ]
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -9,6 +9,9 @@ class IndexView(generic.TemplateView):
 class HeatMapView(generic.TemplateView):
     template_name = "DashboardApp/visualization.html"
 
+class TopicView(generic.TemplateView):
+    template_name = "DashboardApp/topic.html"
+
 def test(request):
     start_day = request.GET['start_day']
     start_hour = request.GET['start_hour']
@@ -17,5 +20,5 @@ def test(request):
     geo_points = Points.objects.filter(day__range=(start_day,end_day)).filter(hour__range=(start_hour,end_hour))
     geo_points = [str(point) for point in geo_points]
     frecuencias=list(Counter(geo_points).most_common())
-    lons, lats, counts = [frecuencia[0].split(',')[0] for frecuencia in frecuencias],[frecuencia[0].split(',')[1] for frecuencia in frecuencias],[1 for frecuencia in frecuencias]
+    lons, lats, counts = [frecuencia[0].split(',')[0] for frecuencia in frecuencias],[frecuencia[0].split(',')[1] for frecuencia in frecuencias],[frecuencia[1] if frecuencia[1] < 30 else 30 for frecuencia in frecuencias]
     return JsonResponse({'lons': lons,'lats': lats, 'counts': counts, 'len': len(frecuencias)})
